c6d435993f095367c213a3d4446ccb6532c6dfdf
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-15 15:02:11 +0000

    Add typer arguments and help strings

--- arcpy2foss/cli.py
+++ arcpy2foss/cli.py
@@ -14,5 +14,9 @@ def hello_world():
 
 
 @app.command()
-def datasets_to_extent(input_files: List[Path], output_file: Path, output_format: str = "GeoJSON"):
+def datasets_to_extent(
+    input_files: List[Path] = typer.Argument(default=None, help="List of 1 or more raster and/or vector datasets"),
+    output_file: Path = typer.Argument(default=None, help="Path to output vector file"),
+    output_format: str = typer.Argument(default="GeoJSON", help="Output vector file format"),
+):
     return extents_to_features(input_files=input_files, output_file=output_file, output_format=output_format)
