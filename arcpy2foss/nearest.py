a9f5b896317529cb73416db5cbb1a85b07da2877
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-16 16:36:00 +0000

    Rename function, add docstring and comments

--- arcpy2foss/nearest.py
+++ arcpy2foss/nearest.py
@@ -3,13 +3,44 @@ from typing import Iterable, Optional
 import geopandas as gpd
 
 
-def nearest_match(
+def nearest_conditional_match_gdf(
     left: gpd.GeoDataFrame,
     right: gpd.GeoDataFrame,
     distance_col: str = "distance",
     match_cols: Optional[Iterable[str]] = None,
     max_distance: Optional[float] = None,
-):
+) -> gpd.GeoDataFrame:
+    """Return rows from ``right`` that are within max_distance and match
+    specific columns of ``left``.
+
+    This function will find all the rows in the ``right`` GeoDataFrame that are
+    within the given ``max_distance`` of the geometries in ``left`` as well as
+    being able to conditionally join on any additional columns; i.e. rows are
+    returned from ``right`` if they are within the given ``max_distance`` AND
+    they also match the values in the specified ``match_cols``.
+
+    Parameters
+    ----------
+    left : gpd.GeoDataFrame
+        Left GeoDataFrame, should be same CRS as ``right``
+    right : gpd.GeoDataFrame
+        Right GeoDataFrame, should be same CRS as ``left``
+    distance_col : str, optional
+        Column to store the distances, by default "distance".
+        Units will the same as the CRS.
+    match_cols : Optional[Iterable[str]], optional
+        Optional list of extra columns to match, by default None.
+        These columns must exist in both ``left`` and ``right``.
+    max_distance : Optional[float], optional
+        Maximum distance for the spatial join, by default None.
+        Units are the same as the CRS so care should be taken if geographic
+        coordinate systems are used.
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        GeoDataFrame containing the matching rows from ``right``.
+    """
     # Find matches from the right gdf that are within the max distance
     right_matches = right.sjoin_nearest(
         left,
@@ -20,15 +51,16 @@ def nearest_match(
         distance_col=distance_col,
     )
 
-    # remove any duplicate columns
+    # remove any duplicate columns (i.e. columns that were added from the left gdf)
     cols_to_drop = [c for c in right_matches.columns if c.endswith("_drop")]
     right_matches = right_matches.drop(columns=cols_to_drop).dropna(subset=[distance_col])
 
-    # rename the suffixed columns that should be kept
+    # rename the suffixed columns that should be kept (i.e. the original columns in the right gdf)
     col_rename_map = {c: c.replace("_keep", "") for c in right_matches.columns if c.endswith("_keep")}
     right_matches = right_matches.rename(columns=col_rename_map)
 
-    # Additional filter based on matching values of specific columns
+    # Additional filter based on matching values of specific columns if any are specified
+    # This is a standard pandas join that does not use geometry
     if match_cols:
         right_matches = right_matches.merge(left[match_cols], how="inner", on=match_cols)
 
--- tests/test_nearest.py
+++ tests/test_nearest.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import pytest
 from shapely.geometry import Point
 
-from arcpy2foss.nearest import nearest_match
+from arcpy2foss.nearest import nearest_conditional_match_gdf
 
 
 @pytest.fixture
@@ -41,30 +41,30 @@ def gdf2():
     )
 
 
-def test_nearest_match(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame):
+def test_nearest_conditional_match_gdf(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame):
     # No conditions, just returns all of the "right" DF with a distance column
-    out = nearest_match(left=gdf1, right=gdf2)
+    out = nearest_conditional_match_gdf(left=gdf1, right=gdf2)
     assert len(out) == 2
     assert "distance" in out.columns
 
 
-def test_nearest_match_with_max_distance(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame):
+def test_nearest_conditional_match_gdf_with_max_distance(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame):
     # Only 1 should be returned that is within the distance
-    out = nearest_match(left=gdf1, right=gdf2, max_distance=0.05)
+    out = nearest_conditional_match_gdf(left=gdf1, right=gdf2, max_distance=0.05)
     assert len(out) == 1
     assert out.id.iloc[0] == "river_tees"
 
 
-def test_nearest_match_with_match_cols(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame):
+def test_nearest_conditional_match_gdf_with_match_cols(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame):
     # Both rows will match as there is no distance specified
     # however only 1 row will match based on using "match_cols"
-    out = nearest_match(left=gdf1, right=gdf2, match_cols=["col1"])
+    out = nearest_conditional_match_gdf(left=gdf1, right=gdf2, match_cols=["col1"])
     assert len(out) == 1
     assert out.id.iloc[0] == "hartlepool"
 
 
-def test_nearest_match_with_match_cols_and_distance(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame):
+def test_nearest_conditional_match_gdf_with_match_cols_and_distance(gdf1: gpd.GeoDataFrame, gdf2: gpd.GeoDataFrame):
     # There are no matches within the distance and having the same values
     # in col1 and col2
-    out = nearest_match(left=gdf1, right=gdf2, match_cols=["col1", "col2"], max_distance=0.1)
+    out = nearest_conditional_match_gdf(left=gdf1, right=gdf2, match_cols=["col1", "col2"], max_distance=0.1)
     assert len(out) == 0
