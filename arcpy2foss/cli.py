2ec3c2ba618ffa002d34950ebd1fd371533c7b8c
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-16 16:44:04 +0000

    Add CLI command and docs for nearest conditional match

--- arcpy2foss/cli.py
+++ arcpy2foss/cli.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 import typer
 
 from arcpy2foss.extent import extents_to_features
 from arcpy2foss.gpx import to_gpx
+from arcpy2foss.nearest import nearest_conditional_match as ncm
 
 app = typer.Typer()
 
@@ -37,3 +38,33 @@ def vector_to_gpx(
     created.
     """
     return to_gpx(input_file=input_file, output_file=output_file)
+
+
+@app.command()
+def nearest_conditional_match(
+    left_vector_file: Path = typer.Argument(default=None, help="Source vector file"),
+    right_vector_file: Path = typer.Argument(default=None, help="Vector file to match with source"),
+    output_file: Path = typer.Argument(default=None, help="Path to output vector file"),
+    output_format: str = typer.Argument(default="GeoJSON", help="Output vector file format"),
+    distance_col: str = typer.Argument(default="distance", help="Name of field to store distance"),
+    match_cols: Optional[List[str]] = typer.Argument(default=None, help="Fields to match, must be in both datasets"),
+    max_distance: Optional[float] = typer.Argument(default=None, help="Distance threshold in same units as vector CRS"),
+):
+    """
+    Match rows from ``right`` that are within max_distance and match specific columns of ``left``.
+
+    This function will find all the rows in the ``right`` dataset that are
+    within the given ``max_distance`` of the geometries in ``left`` as well as
+    being able to conditionally join on any additional columns; i.e. rows are
+    returned from ``right`` if they are within the given ``max_distance`` AND
+    they also match the values in the specified ``match_cols``.
+    """
+    return ncm(
+        left_vector_file=left_vector_file,
+        right_vector_file=right_vector_file,
+        output_file=output_file,
+        output_format=output_format,
+        distance_col=distance_col,
+        match_cols=match_cols,
+        max_distance=max_distance,
+    )
